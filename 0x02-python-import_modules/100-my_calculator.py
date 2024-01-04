4e3a64a5cd1cf5f6195e3ed6b24377bcdd91a678
PaulMuturi <muturipaul10@gmail.com>
2024-01-04 13:12:22 +0000

    100-my_calculator.py

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+if (__name__ == "__main__"):
+    import calculator_1
+    import sys
+
+    argv = sys.argv
+    argv_count = len(argv_count)
+
+    if (argv_count != 4):
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        sys.exit(1)
+
+    a = int(argv[1])
+    op = int(argv[2])
+    b = int(argv[3])
+
+    if op in {'+', '-', '*', '/'):
+        if (op == '+'):
+            calculator_1.add(a, b)
+        if (op == '-'):
+            calculator_1.sub(a, b)
+        if (op == '*'):
+            calculator_1.mul(a, b)
+        if (op == '/'):
+            calculator_1.div(a, b)
+    else:
+        print("Unknown operator. Available operators: +, -, * and /")
+        sys.exit(1)

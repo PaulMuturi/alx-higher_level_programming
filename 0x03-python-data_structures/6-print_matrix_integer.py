cd443afb78b4c8e2556308023f4e0f6af154ad46
PaulMuturi <muturipaul10@gmail.com>
2024-01-06 20:48:23 +0000

    remove space task-6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
-        for i in row:
-            print("{:d}".format(i), end=' ')
+        row_len = len(row)
+        for i in range(row_len):
+            endch = ' '
+            if i == (row_len - 1):
+                endch = ''
+            print("{:d}".format(row[i]), end=endch)
         print()

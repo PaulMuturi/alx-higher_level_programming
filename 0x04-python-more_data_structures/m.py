c279a74c1994586785e32465814c9b6fc5e799fc
PaulMuturi <muturipaul10@gmail.com>
2024-01-10 15:54:08 +0000

    5-number_keys.py revised

--- 0x04-python-more_data_structures/m.py
+++ 0x04-python-more_data_structures/m.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-multiply_list_map = __import__('11-multiply_list_map').multiply_list_map
+number_keys = __import__('5-number_keys').number_keys
 
-my_list = [1, 2, 3, 4, 6]
-new_list = multiply_list_map(my_list, 4)
-print(new_list)
-print(my_list)
+a_dictionary = { 'language': "C", 'number': 13, 'track': "Low level" }
+nb_keys = number_keys(a_dictionary)
+print("Number of keys: {:d}".format(nb_keys))
